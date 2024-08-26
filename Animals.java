c0acfd7ec133b0cfd8ada78fe827c7308997131e
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-26 19:30:33 +0530

    animals

--- Animals.java
+++ Animals.java
@@ -1,4 +1,16 @@
-public interface Animals{
-// Prepare a Functoin called Eat() with String Paramater
-  
+interface Animals{
+
+ public String eat(String s);
+
+}
+class lion implements Animals{
+    public String eat(String s){
+        return s;
+    }
+}
+public class test{
+    public static void main(String[] args) {
+        lion l=new lion();
+        System.out.println( l.eat("meat"));
+    }
 }
--- Emp.java
+++ Emp.java
@@ -1,5 +1,46 @@
 public class Emp{
 // Take 4 Variables of Emp Data With an Access Modifier Private
   // Prepare toString()
+  public class student {
+    private int id;
+    private String name;
+    private long number;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public long getNumber() {
+        return number;
+    }
+    public void setNumber(long number) {
+        this.number = number;
+    }
+    public student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
   
+    public student(int id, String name, long number) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+    }
+    public student(){
+
+    }
+    @Override
+    public String toString() {
+        return "student [id=" + id + ", name=" + name + ", number=" + number + "]";
+    }
+
+    }
 }
